4a7506446d25f9d81ad2a2b3583b5913b8d3bb6d
gaiake12 <gaiake12@hotmail.com>
2025-07-15 20:15:01 -0300

    feat: adding jwt strategy rs256

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { UserPayload } from 'src/auth/jwt-strategy'
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Post()
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
+
+    return 'ok'
+  }
+}
