71167edaed2862d42788fd0b720670b3d4508b4f
gaiake12 <gaiake12@hotmail.com>
2025-08-19 17:16:04 -0300

    feat: creating the fisrt part of schema and question mapper

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,97 +1,99 @@
-import { Slug } from './value-objects/slug'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { Optional } from '@/core/@types/optional'
-import dayjs from 'dayjs'
-import { AggregateRoot } from '@/core/entities/aggregate-root'
-import { QuestionAttachemntList } from './question-attachement-list'
-import { QuestionBestAnswerChosenEvent } from '../events/question-best-answer-chosen-event'
+import { Slug } from "./value-objects/slug";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/@types/optional";
+import dayjs from "dayjs";
+import { AggregateRoot } from "@/core/entities/aggregate-root";
+import { QuestionAttachemntList } from "./question-attachement-list";
+import { QuestionBestAnswerChosenEvent } from "../events/question-best-answer-chosen-event";
 
 export interface QuestionProps {
-  title: string
-  content: string
-  authorId: UniqueEntityID
-  slug: Slug
-  bestAnswerId?: UniqueEntityID
-  attachments: QuestionAttachemntList
-  createdAt: Date
-  updatedAt?: Date
+  title: string;
+  content: string;
+  authorId: UniqueEntityID;
+  slug: Slug;
+  bestAnswerId?: UniqueEntityID | null;
+  attachments: QuestionAttachemntList;
+  createdAt: Date;
+  updatedAt?: Date | null;
 }
 
 export class Question extends AggregateRoot<QuestionProps> {
   get title() {
-    return this.props.title
+    return this.props.title;
   }
 
   get content() {
-    return this.props.content
+    return this.props.content;
   }
 
   get authorId() {
-    return this.props.authorId
+    return this.props.authorId;
   }
 
   get slug() {
-    return this.props.slug
+    return this.props.slug;
   }
 
   get bestAnswerId() {
-    return this.props.bestAnswerId
+    return this.props.bestAnswerId;
   }
 
   get attachments() {
-    return this.props.attachments
+    return this.props.attachments;
   }
 
   get createdAt() {
-    return this.props.createdAt
+    return this.props.createdAt;
   }
 
   get updatedAt() {
-    return this.props.updatedAt
+    return this.props.updatedAt;
   }
 
   get isNew(): boolean {
-    return dayjs().diff(this.props.createdAt) <= 3
+    return dayjs().diff(this.props.createdAt) <= 3;
   }
 
   get exerpt() {
-    return this.props.content.substring(0, 120).trimEnd().concat('...')
+    return this.props.content.substring(0, 120).trimEnd().concat("...");
   }
 
   private touch() {
-    this.props.updatedAt = new Date()
+    this.props.updatedAt = new Date();
   }
 
   set content(content: string) {
-    this.props.content = content
-    this.touch()
+    this.props.content = content;
+    this.touch();
   }
 
-  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined | null) {
     if (
       (bestAnswerId && this.props.bestAnswerId === undefined) ||
       (bestAnswerId && !this.props.bestAnswerId?.equals(bestAnswerId))
     ) {
-      this.addDomainEvent(new QuestionBestAnswerChosenEvent(this, bestAnswerId))
+      this.addDomainEvent(
+        new QuestionBestAnswerChosenEvent(this, bestAnswerId)
+      );
     }
 
-    this.props.bestAnswerId = bestAnswerId
-    this.touch()
+    this.props.bestAnswerId = bestAnswerId;
+    this.touch();
   }
 
   set title(title: string) {
-    this.props.title = title
-    this.props.slug = Slug.createFromText(title)
-    this.touch()
+    this.props.title = title;
+    this.props.slug = Slug.createFromText(title);
+    this.touch();
   }
 
   set attachments(attachments: QuestionAttachemntList) {
-    this.props.attachments = attachments
+    this.props.attachments = attachments;
   }
 
   static create(
-    props: Optional<QuestionProps, 'createdAt' | 'slug' | 'attachments'>,
-    id?: UniqueEntityID,
+    props: Optional<QuestionProps, "createdAt" | "slug" | "attachments">,
+    id?: UniqueEntityID
   ) {
     const question = new Question(
       {
@@ -100,9 +102,9 @@ export class Question extends AggregateRoot<QuestionProps> {
         attachments: props.attachments ?? new QuestionAttachemntList(),
         createdAt: props.createdAt ?? new Date(),
       },
-      id,
-    )
+      id
+    );
 
-    return question
+    return question;
   }
 }
--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -2,10 +2,14 @@ import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
-  create(question: Question): Promise<void> {
+  constructor(private prisma: PrismaService) {}
+
+  async create(question: Question): Promise<void> {
     throw new Error("Method not implemented.");
   }
   delete(question: Question): Promise<void> {
@@ -14,8 +18,18 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   save(question: Question): Promise<Question> {
     throw new Error("Method not implemented.");
   }
-  findById(id: string): Promise<Question | null> {
-    throw new Error("Method not implemented.");
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!question) {
+      return null;
+    }
+
+    return PrismaQuestionMapper.toDomain(question);
   }
   findBySlug(slug: string): Promise<Question | null> {
     throw new Error("Method not implemented.");
