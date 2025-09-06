05cabec9dd00ae2d47c663ad0ec84011e1940440
gaiake12 <gaiake12@hotmail.com>
2025-09-06 14:31:13 -0300

    feat: upload attachment controller

---
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -0,0 +1,40 @@
+import { Test } from "@nestjs/testing";
+import { StudentFactory } from "test/factories/make-student";
+import { AppModule } from "@/infra/app.module";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import request from "supertest";
+import { DatabaseModule } from "@/infra/database/database.module";
+
+describe("Updaload attachment (E2E)", () => {
+  let app: INestApplication;
+  let jwt: JwtService;
+  let studentFactory: StudentFactory;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, JwtService],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    jwt = moduleRef.get(JwtService);
+
+    studentFactory = moduleRef.get(StudentFactory);
+
+    await app.init();
+  });
+
+  test("[POST] /attachments", async () => {
+    const student = await studentFactory.makePrismaStudent();
+
+    const accessToken = await jwt.signAsync({ sub: student.id.toString() });
+
+    const response = await request(app.getHttpServer())
+      .post("/attachments")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .attach("file", "./test/e2e/sample-upload.jpg");
+
+    expect(response.statusCode).toBe(201);
+  });
+});
