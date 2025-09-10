15f8830932869dd3d9c554419dac6af0dea369c1
gaiake12 <gaiake12@hotmail.com>
2025-09-10 17:23:21 -0300

    feat: adding attachments on controller and testing it

--- src/infra/http/controllers/create-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-question.controller.e2e-spec.ts
@@ -6,22 +6,25 @@ import request from "supertest";
 import { JwtService } from "@nestjs/jwt";
 import { StudentFactory } from "test/factories/make-student";
 import { DatabaseModule } from "@/infra/database/database.module";
+import { AttachmentFactory } from "test/factories/make-attachment";
 
 describe("Create question (E2E)", () => {
   let app: INestApplication;
   let prisma: PrismaService;
   let studentFactory: StudentFactory;
+  let attachmentFactory: AttachmentFactory;
   let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory],
+      providers: [StudentFactory, AttachmentFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
 
     studentFactory = moduleRef.get(StudentFactory);
+    attachmentFactory = moduleRef.get(AttachmentFactory);
     prisma = moduleRef.get(PrismaService);
 
     jwt = moduleRef.get(JwtService);
@@ -34,12 +37,16 @@ describe("Create question (E2E)", () => {
 
     const accessToken = jwt.sign({ sub: user.id.toString() });
 
+    const attachment1 = await attachmentFactory.makePrismaAttachment();
+    const attachment2 = await attachmentFactory.makePrismaAttachment();
+
     const response = await request(app.getHttpServer())
       .post("/questions")
       .set("Authorization", `Bearer ${accessToken}`)
       .send({
         title: "Question 1",
         content: "Question Content",
+        attachments: [attachment1.id.toString(), attachment2.id.toString()],
       });
 
     expect(response.statusCode).toBe(201);
@@ -48,8 +55,13 @@ describe("Create question (E2E)", () => {
       where: {
         title: "Question 1",
       },
+      select: {
+        id: true,
+        attachments: true,
+      },
     });
 
     expect(questionOnDatabase).toBeTruthy();
+    expect(questionOnDatabase?.attachments).toHaveLength(2);
   });
 });
