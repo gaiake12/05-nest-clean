e6838e41796e734989ec7c24075e21f8db741df8
gaiake12 <gaiake12@hotmail.com>
2025-09-11 21:26:53 -0300

    feat: adding question details value object

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -3,12 +3,17 @@ import { QuestionsRepository } from "@/domain/forum/application/repositories/que
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { InMemoryQuestionAttachmentsRepository } from "./in-memory-question-attachments-repository";
 import { DomainEvents } from "@/core/events/domain-events";
+import { QuestionDetails } from "@/domain/forum/enterprise/entities/value-objects/question-details";
+import { InMemoryStudentsRepository } from "./in-memory-students-repository";
+import { InMemoryAttachmentsRepository } from "./in-memory-attachments-repository";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = [];
 
   constructor(
-    private inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+    private inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository,
+    private inMemoryStudentsRepository: InMemoryStudentsRepository,
+    private inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
   ) {}
 
   async delete(question: Question) {
@@ -78,5 +83,51 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
       .slice((page - 1) * 20, page * 20);
 
     return questions;
+  }
+
+  async findDetailsBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug);
+
+    if (!question) {
+      return null;
+    }
+
+    const author = this.inMemoryStudentsRepository.items.find((item) =>
+      item.id.equals(question.authorId)
+    );
+
+    if (!author) {
+      throw new Error(`Author with ID ${question.authorId} not found`);
+    }
+
+    const questionAttachments =
+      this.inMemoryQuestionAttachmentsRepository.items.filter((item) =>
+        item.questionId.equals(question.id)
+      );
+
+    const attachments = questionAttachments.map((item) => {
+      const attachment = this.inMemoryAttachmentsRepository.items.find(
+        (attachment) => attachment.id.equals(item.attachmentId)
+      );
+
+      if (!attachment) {
+        throw new Error(`Attachment with ID ${item.attachmentId} not found`);
+      }
+
+      return attachment;
+    });
+
+    return QuestionDetails.create({
+      questionId: question.id,
+      authorId: question.authorId,
+      author: author.name,
+      title: question.title,
+      content: question.content,
+      slug: question.slug,
+      attachments,
+      bestAnswerId: question.bestAnswerId,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    });
   }
 }
